60686f487f331757bb10306abe9a0405da9106a5
Tatyana Kachan <155998480+Tatata1965@users.noreply.github.com>
2024-01-26 22:31:54 +0300

    Update bd_exam.py
    Del comment

--- bd_exam.py
+++ bd_exam.py
@@ -5,7 +5,6 @@ cursor = conn.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS SGL_ (id INTEGER PRIMARY KEY AUTOINCREMENT, Diagn TEXT, Patients integer)""")
 
 
-Diag={'Degener':2,'Inflam':4,'Traum':6}
 k=input("введите диагноз")
 v=int(input('введите количество пациентов'))
 
